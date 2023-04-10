9d469e904ee1ecd7b82aa7b3b403fde7d241349b
phungthanhtu <thanhtucqtuit1952@gmail.com>
2023-04-11 03:33:23 +0700

    socketio implementation

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 const server = require('http').createServer();
-const io = require('socket.io')(server);
+const io = require('socket.io')(server, {
+    cors: {
+      origin: '*',
+      methods: ['GET', 'POST']
+    }
+  });
 const PORT = 5555;
 
 server.listen(PORT);
 
 console.log(`Listening on ${PORT}`);
 
+let readyPlayerCount = 0;
+
 io.on('connection', (socket) => {
-    console.log(`A user connected`);
+    console.log(`A user connected as ${socket.id}`);
+
+    socket.on('ready', () => {
+        console.log(`player ready ${socket.id}`);
+
+        readyPlayerCount++;
+
+        if(readyPlayerCount === 2)
+        {
+            io.emit('startGame', socket.id)
+        }
+
+    })
 });
